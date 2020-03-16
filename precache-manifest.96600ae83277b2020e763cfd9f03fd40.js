1c303c2a007eb2933bbf9f00eecc7695d8200501
sashka0264 <sashka0264@yandex.ru>
2020-03-16 18:52:34 +0300

    Updates

---
+++ precache-manifest.96600ae83277b2020e763cfd9f03fd40.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632cea0c01799c234b169c9d083bb859",
+    "url": "/SamuraiJS/index.html"
+  },
+  {
+    "revision": "a0f8dd98b6c7f3acaa1c",
+    "url": "/SamuraiJS/static/css/10.f4da439a.chunk.css"
+  },
+  {
+    "revision": "0261b83d0f67b4f4d088",
+    "url": "/SamuraiJS/static/css/4.eb0f4384.chunk.css"
+  },
+  {
+    "revision": "072af0875216132f63d6",
+    "url": "/SamuraiJS/static/css/5.62f1ab0a.chunk.css"
+  },
+  {
+    "revision": "4200f07d815b375a1734",
+    "url": "/SamuraiJS/static/css/6.165a1d69.chunk.css"
+  },
+  {
+    "revision": "e8c72e9575890fd2da80",
+    "url": "/SamuraiJS/static/css/7.445f7b82.chunk.css"
+  },
+  {
+    "revision": "362cdab94e8b66c8c8c1",
+    "url": "/SamuraiJS/static/css/8.b7e35740.chunk.css"
+  },
+  {
+    "revision": "f6d1624ec6f962a2253f",
+    "url": "/SamuraiJS/static/css/9.d9a95dc7.chunk.css"
+  },
+  {
+    "revision": "893f27eea059f88250a5",
+    "url": "/SamuraiJS/static/css/main.d5693e2d.chunk.css"
+  },
+  {
+    "revision": "1f136358fd31bd95bcbf",
+    "url": "/SamuraiJS/static/js/0.2bf6d999.chunk.js"
+  },
+  {
+    "revision": "a0f8dd98b6c7f3acaa1c",
+    "url": "/SamuraiJS/static/js/10.c0d35fd5.chunk.js"
+  },
+  {
+    "revision": "114451296df78a981619",
+    "url": "/SamuraiJS/static/js/3.50e47a68.chunk.js"
+  },
+  {
+    "revision": "0261b83d0f67b4f4d088",
+    "url": "/SamuraiJS/static/js/4.bc2f4fd7.chunk.js"
+  },
+  {
+    "revision": "072af0875216132f63d6",
+    "url": "/SamuraiJS/static/js/5.9888f444.chunk.js"
+  },
+  {
+    "revision": "4200f07d815b375a1734",
+    "url": "/SamuraiJS/static/js/6.649fb0b1.chunk.js"
+  },
+  {
+    "revision": "e8c72e9575890fd2da80",
+    "url": "/SamuraiJS/static/js/7.926eecba.chunk.js"
+  },
+  {
+    "revision": "362cdab94e8b66c8c8c1",
+    "url": "/SamuraiJS/static/js/8.786103a0.chunk.js"
+  },
+  {
+    "revision": "f6d1624ec6f962a2253f",
+    "url": "/SamuraiJS/static/js/9.35a903c5.chunk.js"
+  },
+  {
+    "revision": "893f27eea059f88250a5",
+    "url": "/SamuraiJS/static/js/main.d684f8a3.chunk.js"
+  },
+  {
+    "revision": "c61ffa4e3ec8bab7989d",
+    "url": "/SamuraiJS/static/js/runtime~main.045e59db.js"
+  },
+  {
+    "revision": "9fca99c63062888b8ef3fde06decb34b",
+    "url": "/SamuraiJS/static/media/defaultAvatar.9fca99c6.png"
+  },
+  {
+    "revision": "d556ee07d3637e8f202058f191c25100",
+    "url": "/SamuraiJS/static/media/friends.d556ee07.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/SamuraiJS/static/media/heart.e87899ae.svg"
+  },
+  {
+    "revision": "9f631314e4f94b107401e3530c57e75c",
+    "url": "/SamuraiJS/static/media/message.9f631314.svg"
+  },
+  {
+    "revision": "7925fc4bba57f970804e1d85d3f20f97",
+    "url": "/SamuraiJS/static/media/music.7925fc4b.svg"
+  },
+  {
+    "revision": "82e6072b119e45bc292d388c059c9aee",
+    "url": "/SamuraiJS/static/media/news.82e6072b.svg"
+  },
+  {
+    "revision": "0519554c9c1914238587bb49217dc38f",
+    "url": "/SamuraiJS/static/media/profile.0519554c.svg"
+  },
+  {
+    "revision": "1c8a16a2f8501f4587c27241da86bb92",
+    "url": "/SamuraiJS/static/media/sendIcon.1c8a16a2.svg"
+  },
+  {
+    "revision": "23860b1805bc01a18490070b8034de69",
+    "url": "/SamuraiJS/static/media/setting.23860b18.svg"
+  }
+]);
